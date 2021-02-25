d28d2c2adb8f0a1cb6cd18c5f07fa5eb70e69d87
Jimmy Chen <jinny1993820@gmail.com>
2021-02-25 22:04:32 +0800

    Initial Commit

---
+++ RingAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RingAnimation
+//
+//  Created by Jimmy on 2021/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
